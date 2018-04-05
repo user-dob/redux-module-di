fcf8d0305507cdb1b072bb926a5efd01fbd2267b
user-dob <user-dob@yandex.ua>
2018-04-06 00:33:00 +0300

    fix index exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-export * from "./annotation/module";
-export * from "./annotation/component";
-export * from "./interfaces/interfaces";
-export * from "./visitors";
-export * from "./Module";
-export * from "./ModuleBuilder";
-export * from "./bootstrapModule";
+export { module, MODULE_METADATA_KEY } from "./annotation/module";
+export { component } from "./annotation/component";
+export { IAction, ModuleProps, IReducerService, ISagaService, Provider } from "./interfaces/interfaces";
+export { IModuleVisitor, ReducerModuleVisitor, SagaModuleVisitor } from "./visitors";
+export { Module } from "./Module";
+export { ModuleBuilder } from "./ModuleBuilder";
+export { bootstrapModule, createModuleStore, ModuleStore } from "./bootstrapModule";
